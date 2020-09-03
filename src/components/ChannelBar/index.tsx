9afc60bab69251d4228d8437abbfea4954ca1949
swornimbarahi <swornimbarahi@gmail.com>
2020-09-02 20:14:18 -0400

    Completed animation for cross to down and vice verse

--- src/components/ChannelBar/index.tsx
+++ src/components/ChannelBar/index.tsx
@@ -1,4 +1,5 @@
-import React, { FunctionComponent, useContext } from "react";
+import React, { FunctionComponent, useContext, useState } from "react";
+import classnames from "classnames";
 
 import ServerContext from "../../contexts/ServerContext";
 
@@ -11,30 +12,42 @@ const ChannelBar: FunctionComponent = () => {
 	const { selectedServer } = useContext(ServerContext) as {
 		selectedServer: ChatSessionPropType;
 	};
+	const [close, setClose] = useState<boolean>(false);
 
 	return (
 		<div className={styles["channel-bar-container"]}>
-			<div className={styles["channel-bar-header"]}>
+			<div
+				className={styles["channel-bar-header"]}
+				onClick={() => setClose(!close)}
+			>
 				<h1>{selectedServer.serverTitle}</h1>
-				<svg
-					width="18"
-					height="18"
-					className={(styles["down-button"])}
+				<div
+					className={classnames(styles["cross-button-container"],!close && styles["down-button-container"]
+					)}
 				>
-					<g fill="none" fill-rule="evenodd">
-						<path d="M0 0h18v18H0"></path>
-						<path
-							stroke="currentColor"
-							d="M4.5 4.5l9 9"
-							stroke-linecap="round"
-						></path>
-						<path
-							stroke="currentColor"
-							d="M13.5 4.5l-9 9"
-							stroke-linecap="round"
-						></path>
-					</g>
-				</svg>
+					<svg
+						width="18"
+						height="18"
+						className={classnames(
+							styles["down-button"],
+							close && styles["cross-button"]
+						)}
+					>
+						<g fill="none" fill-rule="evenodd">
+							<path d="M0 0h18v18H0"></path>
+							<path
+								stroke="currentColor"
+								d="M4.5 4.5l9 9"
+								stroke-linecap="round"
+							></path>
+							<path
+								stroke="currentColor"
+								d="M13.5 4.5l-9 9"
+								stroke-linecap="round"
+							></path>
+						</g>
+					</svg>
+				</div>
 			</div>
 		</div>
 	);
